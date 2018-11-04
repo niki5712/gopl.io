5688ce6ef8998c54ed9b0c7433c1fb58ffedf0bb
Nikita Orlov <fatemail@mail.ru>
2018-11-04 22:34:57 +0300

    panic vs exception 1

--- ch5/defer2/defer.go
+++ ch5/defer2/defer.go
@@ -1,51 +1,27 @@
-// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
-// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
-
-// See page 151.
-
-// Defer2 demonstrates a deferred call to runtime.Stack during a panic.
 package main
 
 import (
 	"fmt"
-	"os"
-	"runtime"
 )
 
-//!+
 func main() {
-	defer printStack()
+	defer printNiki()
 	f(3)
+	fmt.Println("2niki2")
 }
 
-func printStack() {
-	var buf [4096]byte
-	n := runtime.Stack(buf[:], false)
-	os.Stdout.Write(buf[:n])
+func printNiki() {
+	fmt.Println("!!!niki!!!")
 }
-
-//!-
 
 func f(x int) {
-	fmt.Printf("f(%d)\n", x+0/x) // panics if x == 0
+	defer func() {
+		i := 1
+		if p := recover(); p != nil {
+			fmt.Println(fmt.Errorf("error instead of panic: %v, %v, %v", p, i, x))
+		}
+	}()
 	defer fmt.Printf("defer %d\n", x)
+	fmt.Printf("f(%d)\n", x+0/x) // panics if x == 0
 	f(x - 1)
 }
-
-/*
-//!+printstack
-goroutine 1 [running]:
-main.printStack()
-	src/gopl.io/ch5/defer2/defer.go:20
-main.f(0)
-	src/gopl.io/ch5/defer2/defer.go:27
-main.f(1)
-	src/gopl.io/ch5/defer2/defer.go:29
-main.f(2)
-	src/gopl.io/ch5/defer2/defer.go:29
-main.f(3)
-	src/gopl.io/ch5/defer2/defer.go:29
-main.main()
-	src/gopl.io/ch5/defer2/defer.go:15
-//!-printstack
-*/
---
+++ ch5/defer2/exception.py
@@ -0,0 +1,26 @@
+def main():
+    try:
+        f(3)
+        print("2niki2")
+    finally:
+        print_niki()
+
+
+def print_niki():
+    print("!!!niki!!!")
+
+
+def f(x):
+    try:
+        try:
+            print(f"f({x+0//x})")  # raises if x == 0
+            f(x - 1)
+        finally:
+            print(f"defer {x}")
+            i = 1
+    except Exception as e:
+        print(f"error instead of Exception: {e}, {i}, {x}")
+
+
+if __name__ == '__main__':
+    main()
