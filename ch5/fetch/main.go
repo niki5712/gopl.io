15e6ca801b686ff35b52ba5c7c0a9884b469131d
Nikita Orlov <fatemail@mail.ru>
2018-11-04 22:44:39 +0300

    Exercise 5.18

--- ch5/fetch/main.go
+++ ch5/fetch/main.go
@@ -32,11 +32,13 @@ func fetch(url string) (filename string, n int64, err error) {
 	if err != nil {
 		return "", 0, err
 	}
+	defer func() {
+		// Close file, but prefer error from Copy, if any.
+		if closeErr := f.Close(); err == nil {
+			err = closeErr
+		}
+	}()
 	n, err = io.Copy(f, resp.Body)
-	// Close file, but prefer error from Copy, if any.
-	if closeErr := f.Close(); err == nil {
-		err = closeErr
-	}
 	return local, n, err
 }
 
