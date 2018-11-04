f04d2bde6f07d1f92e681b766bf496ca18e48853
Nikita Orlov <fatemail@mail.ru>
2018-11-04 23:04:51 +0300

    panic vs exception 4

--- ch5/defer2/defer.go
+++ ch5/defer2/defer.go
@@ -15,19 +15,6 @@ func printNiki() {
 }
 
 func f(x int) {
-	defer func() {
-		i := 1
-		if p := recover(); p != nil {
-			fmt.Println(fmt.Errorf("error instead of panic: %v, %v, %v", p, i, x))
-		} else {
-			defer func() {
-				if p := recover(); p != nil {
-					fmt.Println(fmt.Errorf("new error instead of new panic: %v, %v, %v", p, i, x))
-				}
-			}()
-			i = 0 / (i - 1)
-		}
-	}()
 	defer fmt.Printf("defer %d\n", x)
 	fmt.Printf("f(%d)\n", x+0/x) // panics if x == 0
 	f(x - 1)
--- ch5/defer2/exception.py
+++ ch5/defer2/exception.py
@@ -12,19 +12,10 @@ def print_niki():
 
 def f(x):
     try:
-        try:
-            print(f"f({x+0//x})")  # raises if x == 0
-            f(x - 1)
-        finally:
-            print(f"defer {x}")
-            i = 1
-    except Exception as e:
-        print(f"error instead of Exception: {e}, {i}, {x}")
-    else:
-        try:
-            i = 0 // (i - 1)
-        except Exception as e:
-            print(f"new error instead of new Exception: {e}, {i}, {x}")
+        print(f"f({x+0//x})")  # raises if x == 0
+        f(x - 1)
+    finally:
+        print(f"defer {x}")
 
 
 if __name__ == '__main__':
